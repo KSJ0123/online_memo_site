f3497c9059c4614dcb8b4a46784073effcc9fff5
ksj <166187340+KSJ0123@users.noreply.github.com>
2024-11-02 11:56:35 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="Memo_0123",page_icon="📃")
 pages = {
     "Memo" : [
         st.Page("./page/up_memo.py", title="memo"),
--- page/up_memo.py
+++ page/up_memo.py
@@ -1,33 +1,27 @@
 import streamlit as st
 import random as rd
 import sqlite3
+import time
 conn = sqlite3.connect('db_memo.db')
 cursor = conn.cursor()
 memo=st.text_area('Memo',height=1000)
-save=st.button('save')
-conn = sqlite3.connect('db_memo.db')
-cursor = conn.cursor()
+timesleep=False
+save=st.button('save',disabled=st.session_state)
 if save:
+    st.session_state.disabled = False
     while True:
-        memoid=None
-        memo_id=rd.randint(1,4)
+        memo_id=rd.randint(1000,9999)
         cursor.execute(f"SELECT * FROM user WHERE memonum='{memo_id}'")
         row = cursor.fetchone()
         print(memo_id)
-        print(memoid)
-        if row:
-            memoid=row[1]
-            print(memoid)
-        else:
-            memoid=None
-            print(memoid)
-        if memoid != memo_id:
+        if not row:
             sql=f"""
         insert into user(memonum,memo)values('{memo_id}','{memo}')"""
             cursor.execute(sql)
             conn.commit()
             st.success(f'이 메모의 공유 코드는{memo_id}입니다')
             break
+        else:
+            print('다시시도중')
 
-def memo_check():
-    print()
+
