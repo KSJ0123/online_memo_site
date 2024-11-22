10a02ffa534a62f2b6a0eaa0cbe689d3b4546abb
ksj <166187340+KSJ0123@users.noreply.github.com>
2024-11-22 22:38:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.set_page_config(page_title="Memo_0123",page_icon="📃")
 pages = {
     "Memo" : [
-        st.Page("./page/up_memo.py", title="memo"),
-        st.Page("./page/load_memo.py",title="")
+        st.Page("./page/up_memo.py", title="new memo"),
+        st.Page("./page/load_memo.py",title="load memo")
     ]
 }
 pg = st.navigation(pages)
