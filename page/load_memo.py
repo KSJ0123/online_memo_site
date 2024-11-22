9f25d75ac4e1bfdc2eca0d0cbeb4422fd22be15f
ksj <166187340+KSJ0123@users.noreply.github.com>
2024-11-23 05:21:49 +0900

    Update load_memo.py

--- page/load_memo.py
+++ page/load_memo.py
@@ -3,8 +3,8 @@ import sqlite3
 conn = sqlite3.connect('db_memo.db')
 cursor = conn.cursor()
 load_id=st.text_input('공유코드를 입력하세요',max_chars=4)
-if 'load_btn_clik' not in st.session_state:
-    st.session_state.load_btn_clik = False
+if 'load_btn_click' not in st.session_state:
+    st.session_state.load_btn_click = False
 
 if 'load_data' not in st.session_state:
     st.session_state.load_data = None
@@ -13,14 +13,14 @@ if 'load_data' not in st.session_state:
 save_btn=st.button('save')
 
 row = cursor.fetchone()
-if not st.session_state.load_btn_clik:
+if not st.session_state.load_btn_click:
     load_btn=st.button('load')
     if load_btn:
         cursor.execute(f"SELECT * FROM user WHERE memonum='{load_id}'")
         row = cursor.fetchone()
         if row:
             st.session_state.load_data = row[2]
-            st.session_state.load_btn_clik = True
+            st.session_state.load_btn_click = True
         else:
             st.error('공유코드를 정확하게 기입하세요!')
 
