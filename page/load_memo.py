ff4ef4697bf25ceb068f67e50f2605a35e8a38c3
ksj <166187340+KSJ0123@users.noreply.github.com>
2024-11-22 22:34:49 +0900

    Add files via upload

--- page/load_memo.py
+++ page/load_memo.py
@@ -2,29 +2,47 @@ import streamlit as st
 import sqlite3
 conn = sqlite3.connect('db_memo.db')
 cursor = conn.cursor()
-load_data='멋있는 내용을 불러와보세요'
 load_id=st.text_input('공유코드를 입력하세요',max_chars=4)
-load_btn=st.button('load')
+if 'load_btn_clik' not in st.session_state:
+    st.session_state.load_btn_clik = False
+
+if 'load_data' not in st.session_state:
+    st.session_state.load_data = None
+
+
 save_btn=st.button('save')
+
 row = cursor.fetchone()
-if load_btn:
+if not st.session_state.load_btn_clik:
+    load_btn=st.button('load')
+    if load_btn:
+        cursor.execute(f"SELECT * FROM user WHERE memonum='{load_id}'")
+        row = cursor.fetchone()
+        if row:
+            st.session_state.load_data = row[2]
+            st.session_state.load_btn_clik = True
+        else:
+            st.error('공유코드를 정확하게 기입하세요!')
+
+if st.session_state.load_data:
+    load_memo=st.text_area('load memo',st.session_state.load_data,height=1000)
+else:
+    load_memo=None
+
+if save_btn:
     cursor.execute(f"SELECT * FROM user WHERE memonum='{load_id}'")
     row = cursor.fetchone()
     if row:
-        load_data = row[2]
-        load_memo=st.text_area('Memo',load_data,height=1000)
-    else:
-        st.error('공유코드를 정확하게 기입하세요!')
-if save_btn:
-    if row:
-        sql=f"""UPDATE user SET memo ="{load_memo}" WHERE memonum = "{load_id}"
-                """
-        cursor.execute(sql)
-        conn.commit()
-        st.success('저장 되었습니다')
+        if load_memo:
+            sql=f"""UPDATE user SET memo ="{load_memo}" WHERE memonum = "{load_id}"
+                    """
+            cursor.execute(sql)
+            conn.commit()
+            st.success('저장 되었습니다')
+        else:
+            st.error('불러오기 버튼을 먼저 실행해 주세요')
     else:
         st.error('공유코드를 정확하게 기입하세요!')
 
-        
 
 
--- page/up_memo.py
+++ page/up_memo.py
@@ -1,27 +1,40 @@
 import streamlit as st
 import random as rd
 import sqlite3
-import time
+
+# 데이터베이스 연결 및 커서 설정
 conn = sqlite3.connect('db_memo.db')
 cursor = conn.cursor()
-memo=st.text_area('Memo',height=1000)
-timesleep=False
-save=st.button('save',disabled=st.session_state)
-if save:
-    st.session_state.disabled = False
-    while True:
-        memo_id=rd.randint(1000,9999)
-        cursor.execute(f"SELECT * FROM user WHERE memonum='{memo_id}'")
-        row = cursor.fetchone()
-        print(memo_id)
-        if not row:
-            sql=f"""
-        insert into user(memonum,memo)values('{memo_id}','{memo}')"""
-            cursor.execute(sql)
-            conn.commit()
-            st.success(f'이 메모의 공유 코드는{memo_id}입니다')
-            break
-        else:
-            print('다시시도중')
 
 
+memo = st.text_area('Memo', height=1000)
+
+
+if 'button_clicked' not in st.session_state:
+    st.session_state.button_clicked = False
+if 'memo_id' not in st.session_state:
+    st.session_state.memo_id = None
+
+
+if not st.session_state.button_clicked:
+    save = st.button('save', key='save_b')
+    if save:
+        while True:
+            memo_id = rd.randint(1000, 9999)
+            cursor.execute(f"SELECT * FROM user WHERE memonum='{memo_id}'")
+            row = cursor.fetchone()
+            if not row:
+                sql = f"INSERT INTO user (memonum, memo) VALUES ('{memo_id}', '{memo}')"
+                cursor.execute(sql)
+                conn.commit()
+                
+                
+                st.session_state.button_clicked = True
+                st.session_state.memo_id = memo_id
+                break
+            else:
+                print('다시 시도 중')
+
+if st.session_state.memo_id:
+    st.success(f'이 메모의 공유 코드는 {st.session_state.memo_id}입니다.')
+  
